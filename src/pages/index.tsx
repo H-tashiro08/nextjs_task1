0894059e9cc420b4fb575c15dad6e09d514d7c95
Tahono08 <h.tashiro.sys23@morijyobi.ac.jp>
2024-09-02 14:29:16 +0900

    一部を修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter, Tillana } from "next/font/google";
 import Aboutme from "@/components/aboutme";
-import Work from "@/components/Work";
+import Work from "@/components/work";
 import Title from "@/components/title";
 
 const inter = Inter({ subsets: ["latin"] });
